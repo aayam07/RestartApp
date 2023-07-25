0618aa45e31e79535e8e63ca8dd9c1683bc3ddc2
Aayam Adhikari <thisisleo7@gmail.com>
2023-07-25 18:25:57 +0545

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Aayam Adhikari on 25/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
